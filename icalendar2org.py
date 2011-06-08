593f3a942a5296c042e55bbb217ab4b8cf24aa60
Kristoffer Ström <kristoffer.stroem@meetrics.de>
2011-06-08 15:25:38 +0200

    Only list tentative or accepted events, (TODO maybe make this configurable)

--- icalendar2org.py
+++ icalendar2org.py
@@ -5,10 +5,11 @@ import sys
 from vobject import iCalendar, readOne
 
 for x in readOne(sys.stdin).getChildren():
-    if x.name == "VEVENT":
+    if x.name == "VEVENT" and (x.partstat.value == "TENTATIVE" or x.partstat.value == "ACCEPTED"):
 
-        print "* {}\nSCHEDULED: {}\n[[{}][link]]\n{}"\
+        print "* {} -- {}\nSCHEDULED: {}\n[[{}][link]]\n{}"\
             .format(x.summary.value.encode("utf-8"),
+                    x.partstat.value,
                     x.dtstart.value.strftime("<%Y-%m-%d %a>"),
                     x.url.value,
                     x.description.value.encode("utf-8"))
