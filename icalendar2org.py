1ff28d28c600ae1ff1ba178fa26496b7ce9ffad5
Kristoffer Ström <kristoffer.stroem@meetrics.de>
2011-06-08 15:19:59 +0200

    Better formating model

--- icalendar2org.py
+++ icalendar2org.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
+# -*- mode: Python; encoding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+
 import sys
 from vobject import iCalendar, readOne
 
 for x in readOne(sys.stdin).getChildren():
     if x.name == "VEVENT":
-        print "* [[" + x.url.value + "][" + x.summary.value + "]]"
-        print "SCHEDULED: " + x.dtstart.value.strftime("<%Y-%m-%d %a>")
-        print x.description.value
+
+        print "* {}\nSCHEDULED: {}\n[[{}][link]]\n{}"\
+            .format(x.summary.value.encode("utf-8"),
+                    x.dtstart.value.strftime("<%Y-%m-%d %a>"),
+                    x.url.value,
+                    x.description.value.encode("utf-8"))
